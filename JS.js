301203f89028dee1b6a2589998882249753077df
Unknown <killyan.hubert@gmail.com>
2017-12-04 11:33:18 +0100

    Création du contexte WebAudio et VICTOIRE du son qui sort par les haut-parleurs a fréquences différentes en fonction de la touche appuyée sur le Clavier.

--- JS.js
+++ JS.js
@@ -1,27 +1,69 @@
 window.onload = init; 
 let canvas, ctx;
+// LES DEUX PROCHAINES LIGNES PERMETTENT D'INITIALISER LE CONTEXTE AUDIO. 
+var audioCtx = window.AudioContext || window.webkitAudioContext;
+var audioContext;
 
     function init(){
         console.log("PAGE CHARGéE");
 
+        audioContext = new audioCtx();
         canvas = document.querySelector("#CanvasPiano");
 
         ctx = canvas.getContext("2d");
-
+      
         drawPiano();
         InputTouche();
-    }    
+    } 
 
     function InputTouche(){
         window.addEventListener('keydown', function(event){
+         //l'oscillateur permet de "creer" virtuellement un son a partir d'une sinusoidale
+        var oscillateur = audioContext.createOscillator();
+        //le gain servira au volume  
+        var noeudGain = audioContext.createGain();
+      
+        //Connection aux speakers
+        //La methode connect permet de lier les différents noeuds (étapes du son (effets, gain, etc..))dans l'ordre d'écriture 
+        oscillateur.connect(noeudGain);    
+        noeudGain.connect(audioContext.destination);
             switch(event.keyCode){
-                case 65: console.log("DO"); break;
-                case 90: console.log("RE"); break;
-                case 69: console.log("MI"); break;
-                case 82: console.log("FA"); break;
-                case 84: console.log("SOL"); break;
-                case 89: console.log("LA"); break;
-                case 85: console.log("SI"); break;
+                case 65: 
+                console.log("DO");
+                oscillateur.type='sine';//Definini l'oscillateur comme fonctionnant grace a une sinusoïdale !
+                oscillateur.frequency.value = 261,63;
+                oscillateur.start();
+                break;
+                case 90: console.log("RE");
+                oscillateur.type='sine';
+                oscillateur.frequency.value = 293,66;
+                oscillateur.start();
+                 break;
+                case 69: console.log("MI"); 
+                oscillateur.type='sine';
+                oscillateur.frequency.value = 329,63;
+                oscillateur.start();
+                break;
+                case 82: console.log("FA"); 
+                oscillateur.type='sine';
+                oscillateur.frequency.value = 349,23;
+                oscillateur.start();
+                break;
+                case 84: console.log("SOL");
+                oscillateur.type='sine';
+                oscillateur.frequency.value = 392,00;
+                oscillateur.start();
+                 break;
+                case 89: console.log("LA");
+                oscillateur.type='sine';
+                oscillateur.frequency.value = 440,00;
+                oscillateur.start();
+                 break;
+                case 85: console.log("SI"); 
+                oscillateur.type='sine';
+                oscillateur.frequency.value = 493,88;
+                oscillateur.start();
+                break;
             }
         }, false)
     }
